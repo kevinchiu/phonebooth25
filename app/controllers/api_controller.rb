63fddcbdd86f66a53742a8f329f7411c627b9733
Kevin Chiu <kevin@kevinchiu.org>
2010-10-11 00:30:33 -0400

    added test api

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,4 +1,5 @@
 class ApiController < ApplicationController
+  require 'twiliolib'
   #/api/q=what does bark taste like
   def ask
     @q = params[:q]
@@ -6,4 +7,19 @@ class ApiController < ApplicationController
     @r.addSay "What does bark taste like?"
     render :xml => @r.respond
   end
+  
+  def test
+    account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+
+    d = {
+        'From' => CALLER_ID,
+        'To' => '6503532703',
+        'Url' => 'http://demo.twilio.com/welcome',
+    }
+    resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/Calls",
+        'POST', d)
+    resp.error! unless resp.kind_of? Net::HTTPSuccess
+    puts "code: %s\nbody: %s" % [resp.code, resp.body]
+  end
+  
 end
