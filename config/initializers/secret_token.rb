bfe2cf4d3e5b1b20f603e17ac46c6e5a3416aa33
Kevin Chiu <kevin@kevinchiu.org>
2010-10-10 23:39:48 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebooth25::Application.config.secret_token = '9f1c60b5e800f4282474a4a22ab89a69e44084eb61a5cc0144cb0b12943681f51b0f0f83abe2530d1d632e6ebd2a7578100848fd172609bf24c5cda85936eb1f'
