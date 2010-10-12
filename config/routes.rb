bbe4c565d0c75d15f6585b48710416d4d6b2c4e1
Kevin Chiu <kevin@kevinchiu.org>
2010-10-11 23:07:15 -0400

    added q routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,10 @@ Phonebooth25::Application.routes.draw do
   resources :transcripts
 
     match 'api/ask_question', :controller => 'api', :action => 'ask_question'
-    match 'api/ask', :controller => 'api', :action => 'ask', :conditions => {:method => :post}
     match 'api/save_transcript' , :controller => 'api', :action => 'save_transcript'
     match 'api/multicall' , :controller => 'api', :action => 'multicall'
+    match 'api/ask', :controller => 'api', :action => 'ask', :conditions => {:method => :post}
+    match 'api/ask', :controller => 'api', :action => 'ask2', :conditions => {:method => :post}
+    match 'api/ask', :controller => 'api', :action => 'ask3', :conditions => {:method => :post}
     
 end
