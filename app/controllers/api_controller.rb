5069eac95e0a0b733a54b9342e1800d122df7eef
Kevin Chiu <kevin@kevinchiu.org>
2010-10-11 23:02:32 -0400

    trying redirects for multiple questions

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -20,17 +20,26 @@ class ApiController < ApplicationController
     render :nothing => true
   end
   
+  def ask3
+    r = Twilio::Response.new
+    r.addPlay "/q3.wav"
+    r.addRecord({:finishOnKey => "1", :playBeep => "false", :transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 10, :maxLength => 30})
+    render :xml => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + r.respond
+  end
+  
+  def ask2
+    r = Twilio::Response.new
+    r.addPlay "/q2.wav"
+    r.addRecord({:finishOnKey => "1", :playBeep => "false", :transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 10, :maxLength => 30})
+    r.addRedirect({:method => "#{SERVER}/ask3"})
+    render :xml => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + r.respond
+  end
+  
   def ask
     r = Twilio::Response.new
     r.addPlay "/q1.wav"
-    r.addRecord({:finishOnKey => "1", :playBeep => "false", :transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 3, :maxLength => 30})
-    r.addPlay "/q2.wav"
-    # r.addRecord({:transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 29, :maxLength => 30})
-    
-    # r.addRecord({:transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 29, :maxLength => 30})
-    # r.addPlay "/q3.wav"
-    # r.addRecord({:transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 29, :maxLength => 30})
-    r.addHangup
+    r.addRecord({:finishOnKey => "1", :playBeep => "false", :transcribe => true, :transcribeCallback => "#{SERVER}/api/save_transcript?question=hi", :timeout => 10, :maxLength => 30})
+    r.addRedirect({:method => "#{SERVER}/ask2"})
     render :xml => "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n" + r.respond
   end
   
